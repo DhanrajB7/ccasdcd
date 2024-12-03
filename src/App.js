2bda77ede62939e5dd2318ff18acaf0d17371c17
DhanrajB7 <dhanrajbhalala8462@gmail.com>
2024-12-03 08:19:12 -0500

    1

---
+++ src/App.js
@@ -0,0 +1,27 @@
+// App.js
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Home />
+            <About />
+            <Skills />
+            <Experience />
+            <Projects />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
